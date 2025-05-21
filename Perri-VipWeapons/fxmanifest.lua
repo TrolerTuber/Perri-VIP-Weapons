e9c14a937efeb4a8bd517429753fa550ef93ba68
Perri <94766847+TrolerTuber@users.noreply.github.com>
2025-05-21 18:41:17 +0200

    Fixxed error in MenuV

--- Perri-VipWeapons/fxmanifest.lua
+++ Perri-VipWeapons/fxmanifest.lua
@@ -1,25 +1,22 @@
 fx_version 'cerulean'
 game 'gta5'
 lua54 'yes'
-fx_version '0.0.1'
+fx_version '0.0.2'
 
 author 'PerriTuber'
 description 'http://scripts.perrituber.xyz/'
 
-client_scripts { 'client/main.lua', 'client/exports.lua', 'client/menus/menu.lua', 'client/classes/player.lua'}
+client_scripts { '@menuv/menuv.lua', 'client/main.lua', 'client/exports.lua', 'client/menus/menu.lua', 'client/classes/player.lua'}
 
 server_scripts {'server/main.lua', 'server/logs.lua', 'server/callbacks.lua' }
 
 escrow_ignore { 'client/exports.lua', 'shared/*.lua', 'server/logs.lua' }
 
 shared_scripts {
-  '@menuv/menuv.lua',
   '@es_extended/imports.lua',
   '@ox_lib/init.lua',
   'shared/*.lua'
 }
-
-
 
 ui_page 'ui/index.html'
 
